d1374ef24c7715711ad51b922ddcaf751691370c
Alexzander Hurd <ajghurd@gmail.com>
2025-07-31 13:17:10 +0100

    Explicitly added Feed Id into Entry model

---
+++ Migrations/20250731120420_add missing foreign key.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RSS_Reader.Migrations
+{
+    /// <inheritdoc />
+    public partial class addmissingforeignkey : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Entries_Feeds_FeedId",
+                table: "Entries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FeedId",
+                table: "Entries",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Entries_Feeds_FeedId",
+                table: "Entries",
+                column: "FeedId",
+                principalTable: "Feeds",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Entries_Feeds_FeedId",
+                table: "Entries");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "FeedId",
+                table: "Entries",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Entries_Feeds_FeedId",
+                table: "Entries",
+                column: "FeedId",
+                principalTable: "Feeds",
+                principalColumn: "Id");
+        }
+    }
+}
--- Models/DataModels/EntryDataModel.cs
+++ Models/DataModels/EntryDataModel.cs
@@ -6,6 +6,7 @@ public class Entry
 {
     [Key]
     public string Id { get; set; }
+    public string FeedId { get; set; }
     public string? Title { get; set; }
     public string? PubDate { get; set; }
     public string? Link { get; set; }
